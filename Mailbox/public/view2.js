752dd1af1d0f84f21706b51e2f7fa64a4bb6c461
mubeentauni <mubeentauni@gmail.com>
2015-06-23 22:40:11 +0500

    mailbox k shaskay

--- Mailbox/public/view2.js
+++ Mailbox/public/view2.js
@@ -9,7 +9,7 @@
 		 	var timestamp=new Date(data.timestamp);
 		 	var dt=timestamp.getHours() +":" + timestamp.getMinutes();
 
-		 	var inb=$('<a href="#" class="list-group-item"><span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;display: inline-block;">'+data.to+'</span> <br><span class="">'+data.subject+'</span><span class="text-muted" style="font-size: 11px;">- '+data.message+'</span> <span class="badge">'+dt+'</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip"></span></span></a>');
+		 	var inb=$('<div class="clk" id='+data._id+' onclick="funcu(this)"> <a  class="list-group-item "><span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;display: inline-block;">'+data.to+'</span> <br><span class="">'+data.subject+'</span><span class="text-muted" style="font-size: 11px;">- '+data.message+'</span> <span class="badge">'+dt+'</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip"></span></span></a><div class="showdiv" style="display:none";><label>To</label><input type="text" class="form-control" id="from" name="from" value='+data.to+' disabled="disabled" style="background:black;color:white;"><label>Subject</label><input type="text" class="form-control" id="subject" name="from" value='+data.subject+' disabled="disabled" style="background:black;color:white;"><label>Message</label><input type="text" class="form-control" id="from" name="message" value='+data.message+' disabled="disabled" style="background:black;color:white;"></div></div>');
 		 				//$("#time").append(dt);
 		 				//$("#from").append(from);
 		 				//$("#subject").append(subject);
@@ -20,3 +20,18 @@
 }
 var ui=new updateUi();
 
+function funcu(kuch){
+alert("hi");
+
+//debugger;
+$(kuch).find(".showdiv").toggle();
+
+
+	//var hhh=$('<input type="text" value="3" class="field left" readonly="readonly" >');
+	//$(kuch).append(hhh);
+	//throw new Error("Something went badly wrong!");
+//var abc=$(kuch).attr("id");
+//	console.log("abca",abc);
+	//var ghn=getfrom(abc);
+	//console.log("kisne bheja",ghn);
+}
